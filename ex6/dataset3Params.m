a039e26cf9651c5bc28cca86f4963a2a4f5a2dfe
Sarthak Girdhar <sg.sarthakgirdhar@gmail.com>
2016-04-18 08:46:23 +0530

    Update dataset3Params.m

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -26,8 +26,8 @@ sigma = 0.3;
 
 values = [0.01 0.03 0.1 0.3 1 3 10 30];
 min_err = 1;
-for i=1:8,
-    for j=1:8,
+for i=1:8
+    for j=1:8
         C_test = values(i);
         sigma_test = values(j);
         model = svmTrain(X, y, C_test, @(x1, x2) gaussianKernel(x1, x2, sigma_test));
